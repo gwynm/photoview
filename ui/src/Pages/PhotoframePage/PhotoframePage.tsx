7e1cf9c4737f46ec375ecb834f7874676dd3e823
Gwyn Morfey <mail@gwynmorfey.com>
2022-02-21 09:52:20 +1030

    Working photoframe

--- ui/src/Pages/PhotoframePage/PhotoframePage.tsx
+++ ui/src/Pages/PhotoframePage/PhotoframePage.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import AlbumBoxes from '../../components/albumGallery/AlbumBoxes'
-import Layout from '../../components/layout/Layout'
+import React, { useEffect, useState } from 'react'
 import { useQuery, gql } from '@apollo/client'
 import {
   myTimeline, myTimelineVariables
 } from '../../components/timelineGallery/__generated__/myTimeline'
+import PresentView from '../../components/photoGallery/presentView/PresentView'
 
 /* Reuse timeline query to get all media */
 const MAX_PHOTOFRAME_SET_SIZE = 9999;
+const PHOTOFRAME_RELOAD_MSEC = 12 * 60 * 60 * 1000; //12 hours
+const PHOTOFRAME_FLIP_MSEC = 60 * 1000; //1 minute
+
 const MY_TIMELINE_QUERY = gql`
   query myTimeline(
     $onlyFavorites: Boolean
@@ -51,9 +53,19 @@ const MY_TIMELINE_QUERY = gql`
   }
 `
 
-
 const PhotoframePage = () => {
-  const { data, error } = useQuery<
+
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const reloadTimer = setTimeout(() => {
+      console.log('Reloading page to get latest code updates (and new photos), if any');
+      location.reload();
+    }, PHOTOFRAME_RELOAD_MSEC);
+    return () => clearTimeout(reloadTimer);
+  }, []);
+
+  const { data } = useQuery<
     myTimeline,
     myTimelineVariables
   >(MY_TIMELINE_QUERY, {
@@ -65,9 +77,24 @@ const PhotoframePage = () => {
     },
   })
 
+  useEffect(() => {
+    const flipTimer = setInterval(() => {
+      if (data?.myTimeline) {
+        const newIndex = Math.floor(Math.random() * data.myTimeline.length);
+        setCurrentPhotoIndex(newIndex);
+      }
+    }, PHOTOFRAME_FLIP_MSEC);
+    return () => clearInterval(flipTimer);
+  }, []);
+
+  const currentPhoto = data && data.myTimeline[currentPhotoIndex];
   return (
     <div>
       This is a list of all your media. We have {data?.myTimeline.length} items.
+      {currentPhoto && <PresentView
+        activeMedia={currentPhoto}
+        dispatchMedia={() => null}
+      />}
     </div>
   )
 }
