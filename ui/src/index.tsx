5e68738ba6b09a367188554946485c5e10476b55
Gwyn Morfey <mail@gwynmorfey.com>
2022-02-22 11:11:56 +1030

    Kill service worker
    We don't care about the speed boost, we don't work offline anyway (unlikely that api responses and photo highres images will be in cache), and service workers make development, debugging, and production updates very difficult.

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -8,7 +8,6 @@ import { ApolloProvider } from '@apollo/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { setupLocalization } from './localization'
 import { updateTheme } from './theme'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import './index.css'
 import { SidebarProvider } from './components/sidebar/Sidebar'
@@ -27,5 +26,3 @@ const Main = () => (
 )
 
 ReactDOM.render(<Main />, document.getElementById('root'))
-
-serviceWorkerRegistration.register()
